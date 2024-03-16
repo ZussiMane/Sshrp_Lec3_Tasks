4988605e54da3b656a4a9710aabb0eea1511a68c
ZussiMane <igoryunja@gmail.com>
2024-03-16 18:40:30 +0300

    Добавил условие 2 задачи

--- Program.cs
+++ Program.cs
@@ -3,13 +3,17 @@
 // a, b, c, d, - некоторые целый числа. 
 // Решение оформить в виде функции.
 
-double CalculateFormula(int a, int b, int c, int d)
-{
-    double numenator = a * b;
-    int denomenator = c + d;
-    double result = numenator / denomenator;
-    return result;
-}
+// double CalculateFormula(int a, int b, int c, int d)
+// {
+//     double numenator = a * b;
+//     int denomenator = c + d;
+//     double result = numenator / denomenator;
+//     return result;
+// }
 
-double result = CalculateFormula(1, 2, 3, 4);
-Console.WriteLine(result);
+// double result = CalculateFormula(1, 2, 3, 4);
+// Console.WriteLine(result);
+//------------------------------------------------//
+// Задача 2
+// Вывлд на экран квадратов числа от 1 до N.
+
